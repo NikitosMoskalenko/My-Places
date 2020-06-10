45da5fd332142ee76012ac938503678c7d769de4
Moskalenko Nikita <n.moskalneko1996@yahoo.com>
2020-06-10 21:52:04 +0300

    Created rating control and logic

---
+++ RatingControl/Control/RatingControl.swift
@@ -0,0 +1,14 @@
+//
+//  RatingControl.swift
+//  MyPlaces
+//
+//  Created by Nikita Moskalenko on 6/10/20.
+//  Copyright © 2020 Nikita Moskalenko. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+
+}
