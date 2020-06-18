03a1789a688f26b84a32f3a54098bc04b69021f2
Moskalenko Nikita <n.moskalneko1996@yahoo.com>
2020-06-18 12:48:09 +0200

    Direction

--- MyPlaces/MyPlacesTableView/Modal/PlaceCellModel.swift
+++ MyPlaces/MyPlacesTableView/Modal/PlaceCellModel.swift
@@ -14,12 +14,14 @@ final class PlaceCellModel: Object {
     @objc dynamic var location: String?
     @objc dynamic var imageData: Data?
     @objc dynamic var date = Date()
+    @objc dynamic var rating = 0.0
     
-    convenience init(title: String, type: String?, location: String?, imageData: Data?) {
+    convenience init(title: String, type: String?, location: String?, imageData: Data?, rating: Double) {
         self.init()
         self.title = title
         self.type = type
         self.location = location
-        self.imageData = imageData 
+        self.imageData = imageData
+        self.rating = rating
     }
 }
